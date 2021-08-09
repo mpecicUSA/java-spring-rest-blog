872287c2740738d97b773bb4af1d1fb0dc38e755
Marko <mpecic2010@gmail.com>
2021-08-09 17:00:20 +0200

    add jpa

--- src/main/java/com/pluralsight/blog/data/PostRepository.java
+++ src/main/java/com/pluralsight/blog/data/PostRepository.java
@@ -4,7 +4,11 @@ import com.pluralsight.blog.model.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RestResource;
 
+import java.util.List;
+
 
 public interface PostRepository extends JpaRepository<Post,Long> {
 
+    @RestResource(rel="contains-title", path="containsTitle")
+    List<Post> findByTitleContaining(String title);
 }
